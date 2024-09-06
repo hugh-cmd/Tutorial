3797390e730412d3f1f69ceb182cc098c61c051d
Yiyou zhang <anzyy2003@qq.com>
2024-09-06 17:03:18 +0800

    Added feature 1

--- Greeter.java
+++ Greeter.java
@@ -13,13 +13,12 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + name;
+    return messages.getString("greeting") + ", " + name.toUpperCase();
   }
 
   public static void main(String[] args) {
     String language = args[0];
     String country = args[1];
-
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
   }
